6c1729637b56471297f5c133886ea70b43d17249
Subha-665 <subhapratha015@gmail.com>
2026-09-23 11:26:24 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -7,5 +7,9 @@
 # =====================================
 
 # Write your commands below
-
-
+rm -f /swapfile
+dd if=/dev/zero of=/swapfile bs=1M count=1024
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+swapon --show
